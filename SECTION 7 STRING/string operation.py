29e9649fd219edfc689ffce3c1057ee04948af70
mahesh0842 <mahesh767000@gmail.com>
2025-02-06 03:16:23 +0530

    string

--- SECTION 7 STRING/string operation.py
+++ SECTION 7 STRING/string operation.py
@@ -1,6 +1,16 @@
 s1='hello'
 s2 = 'world'
-s3 = s1+s2
+'''s3 = s1+s2
 print(s3)
 print(s1*3)
-print()
+for x in s1:
+    print(x)'''
+for i in range(0,len(s1)):
+    print(s1[i])
+print("new")
+s3 = "hello world"
+for x in range(0,len(s3),2):
+    print(s3[x])
+print("slicing")
+s4 = 'hello world'
+s4[::1]
