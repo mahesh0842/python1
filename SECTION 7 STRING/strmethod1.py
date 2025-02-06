49ce99fa64f4ba8368a99f42dd8096ec476111f2
mahesh0842 <maheshky2140@gmail.com>
2025-02-06 14:12:56 +0530

    string method find if we using method we called methd using method .

---
+++ SECTION 7 STRING/strmethod1.py
@@ -0,0 +1,9 @@
+s='hello how are you'
+print(s.find('how'))
+print(s.find('k'))
+print(s.find('o'))
+print(s.find('o',5))
+print(s.find('o',8,16))
+print(s.rfind('o'))#its start from right to left
+print(s.index('how'))
+
