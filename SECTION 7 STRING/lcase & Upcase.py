920e3409124c10a0eb86c8d55a758a7ddf21d24b
mahesh0842 <maheshky2140@gmail.com>
2025-02-06 16:59:23 +0530

    lcase and ucase

---
+++ SECTION 7 STRING/lcase & Upcase.py
@@ -0,0 +1,16 @@
+#upeer and lowercase of string
+s='hello how are you'
+#print(s.capitalize())
+#print(s.lower())
+print(s.swapcase())
+s1='Hello how aRe yoU'
+print(s1.title())
+print(s1.casefold())
+s2= 'helllo'
+s3='Hello'
+print(s2==s3)
+if s2.lower()==s3.lower():
+    print('yes')
+s4='\u01C6'
+print(s4)
+print(s4.upper)
