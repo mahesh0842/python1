ff850acd2256d598908378a927f88eb1e3fb3582
mahesh0842 <maheshky2140@gmail.com>
2025-02-06 14:18:12 +0530

    rindex,count

--- SECTION 7 STRING/strmethod1.py
+++ SECTION 7 STRING/strmethod1.py
@@ -6,4 +6,5 @@ print(s.find('o',5))
 print(s.find('o',8,16))
 print(s.rfind('o'))#its start from right to left
 print(s.index('how'))
-
+print("it comes how many times",s.rindex('l'))
+print(s.count('o'))
