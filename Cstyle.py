79215ca7926ff0839634d31988f2e7e3b0b21e49
mahesh0842 <maheshky2140@gmail.com>
2025-02-08 20:06:04 +0530

    python style printing

--- Cstyle.py
+++ Cstyle.py
@@ -2,3 +2,6 @@ rollno=10
 Name= 'ravi'
 avg=86.89
 print('student roll no is %d, his name %s and avg is %f his future is bright'%(rollno,Name,avg))
+name='smith kumar '
+marks=86.7612345 
+print('%2.5f'%marks)
---
+++ Section 8/pythonstyle.py
@@ -0,0 +1,5 @@
+#python style printing
+a=22
+b=7
+c=22/7
+print("Division of {0:<10} and {1:^15} is {2:2.4}".format(a,b,c))
