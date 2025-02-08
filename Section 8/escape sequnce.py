83a987708e7e084a6a665f3f9ec8b0c8dc56a9e1
mahesh0842 <maheshky2140@gmail.com>
2025-02-08 18:49:53 +0530

    sub super

--- Section 8/escape sequnce.py
+++ Section 8/escape sequnce.py
@@ -3,3 +3,10 @@ print('ab\avcdefghi',end ='')
 print('\t\t\t welcome')
 print('\t\t\tjohn')
 print('\t\t\tsmith\n\n\n singh')
+print(r"abc\nmksss")
+s='\N{rupee sign}'
+print(s)
+s1='\N{subscript seven}'
+print(s1)
+s2='\N{superscript five}'
+print(s2)
