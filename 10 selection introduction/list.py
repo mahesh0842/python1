252628fdea3d2e90afb3019bed2c14a1975a9288
mahesh0842 <53919354+mahesh0842@users.noreply.github.com>
2025-02-13 16:07:53 +0530

    list challenges

--- 10 selection introduction/list.py
+++ 10 selection introduction/list.py
@@ -1,5 +1,11 @@
 mylist=['john','smith','yadav',12,23]
 mylist1=list((1,2,3,4,5))#calling  the list function
 print(mylist[2])
+print(mylist)
 print(mylist[-1])
 print(mylist1)
+mylist[0]='alpha'
+print(mylist)
+print(len(mylist))
+mylist.append[78]
+print(mylist)
