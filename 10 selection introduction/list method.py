448bd2fbdc479f3d875c89d84fe0346bdbbfd17d
mahesh0842 <53919354+mahesh0842@users.noreply.github.com>
2025-02-13 19:16:22 +0530

    cloning

--- 10 selection introduction/list method.py
+++ 10 selection introduction/list method.py
@@ -2,8 +2,14 @@ l1=[1,2,3,4666,8,88]
 print(len(l1))
 l1.append(111)
 print(l1)
+print(id(l1))#print address of list
 print(len(l1))
 l1.extend([1,11,23,43])
 print(l1,'extend')
 l1.insert(-5,1000)
 print(l1,'INSERT AT INDEX -5')
+print(id(l1))#print address of list
+#copy method like cloning
+l2=l1.copy()
+print(l2)
+print(id(l2))
