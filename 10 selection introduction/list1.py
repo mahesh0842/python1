5d12edfa578994d9289e6462291686c478f02d87
mahesh0842 <53919354+mahesh0842@users.noreply.github.com>
2025-02-13 16:36:14 +0530

    list

--- 10 selection introduction/list1.py
+++ 10 selection introduction/list1.py
@@ -1,5 +1,5 @@
 #index operator
-list1=[1,2,3,4,5,6,7]
+list1=[1,2,3,4,5,55,7]
 print(list1)
 print(list1[-5])
 x=list1[3]
@@ -10,3 +10,8 @@ print(list1)
 print(list1[2:5])
 print(list1[0:8:2],'START,END,STEP')
 print(list1[1:-1],'reverse list printing')
+print(list1[-1:-3:-1],'rev')
+for i in list1:
+    print('loop',i)
+for item in range(len(list1)-1,-1,-1):
+    print(list1[item])
