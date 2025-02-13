9583aac96864f3c3b58efec8f99bd2e9e8a6cba3
mahesh0842 <53919354+mahesh0842@users.noreply.github.com>
2025-02-13 16:59:16 +0530

    operation

--- 10 selection introduction/opration on list.py
+++ 10 selection introduction/opration on list.py
@@ -1,4 +1,14 @@
 list1=[1,222,3,4,5,6,7,899]
+list2=['john','smith','ways']
 list1[0:2]=[2345,55]
 print(list1)
-
+list3=list1+list2
+print(list3)
+if 899 in list1:
+    print("found")
+else:
+    print('not found')
+if 899 not in list1:
+    print("found")
+else:
+    print('not foundddd')
