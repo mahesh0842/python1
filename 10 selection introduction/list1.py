d1fa3558f33db032e9e60ea1fe0b252933f9ac76
mahesh0842 <53919354+mahesh0842@users.noreply.github.com>
2025-02-13 16:18:48 +0530

    slicing

---
+++ 10 selection introduction/list1.py
@@ -0,0 +1,12 @@
+#index operator
+list1=[1,2,3,4,5,6,7]
+print(list1)
+print(list1[-5])
+x=list1[3]
+list1[4]=88#modified the list
+print(x)
+print(list1)
+#slicing of list
+print(list1[2:5])
+print(list1[0:8:2],'START,END,STEP')
+print(list1[1:-1],'reverse list printing')
