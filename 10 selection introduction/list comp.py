949d1f7b177fb3b102d218728e543eab7c68987f
mahesh0842 <53919354+mahesh0842@users.noreply.github.com>
2025-02-14 17:15:11 +0530

    LIST STYLE FOR WRITING

---
+++ 10 selection introduction/list comp.py
@@ -0,0 +1,19 @@
+l1=[]
+for i in range(10):
+    l1.append(i)
+print(l1)
+#or
+l2=[i for i in range(12)]
+print(l2)
+#or
+l3=[x**2 for x in range(1,8)]
+print(l3)
+#or
+l4=[x for x in (10,12,13,15,16)if x%2==0]
+print(l4)
+#0r
+l5=[x.lower() for x in 'pytHon']
+print(l5)
+#or
+l6=[x for x in 'abc%412345de@fgh' if x.isalpha()]
+print(l6)
