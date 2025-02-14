74e7f226bb6c8f8df41af341b05b417b696bfe72
mahesh0842 <53919354+mahesh0842@users.noreply.github.com>
2025-02-14 17:01:17 +0530

    sorting

--- 10 selection introduction/index&sorting.py
+++ 10 selection introduction/index&sorting.py
@@ -1,4 +1,10 @@
-l1=[1,2,3,4,55,66,7744,777,33]
-print(l1.index(55))
+l1=[24631,2,7777,4,55,66,7744,777,33]
+"""print(l1.index(55))
 print(l1.index(55,2,8))#element start end
+print(l1)"""
+sorted(l1)
+print(l1)
+l1.sort()
+print(l1)
+l1.sort(reverse=True)
 print(l1)
