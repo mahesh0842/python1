6f47a4d8ee9034db59a972282730b7aaa4f5dd51
mahesh0842 <53919354+mahesh0842@users.noreply.github.com>
2025-02-16 03:11:54 +0530

    set can grow

--- 12 sets/first.py
+++ 12 sets/first.py
@@ -1,3 +1,14 @@
 s1={"mk",23,23.5,5+4j,'True','mk'}
 print(type(s1))
 print(s1,"no duplicate prints")
+s2 = set((1,2,3,4,55))
+print(type(s2))
+print(s2)
+#unorder 
+s3=set('python')
+print(s3)
+#adding value
+s4=(10,20,30,40,50)
+s4 = s4+(10000,)
+print(s4)
+print(len(s4))
