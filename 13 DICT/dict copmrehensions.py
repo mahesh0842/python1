cf1e93baed00d78fce76f7e31506ccda6869036f
mahesh0842 <53919354+mahesh0842@users.noreply.github.com>
2025-02-17 02:04:38 +0530

    aceess,insert,update,delete

--- 13 DICT/dict copmrehensions.py
+++ 13 DICT/dict copmrehensions.py
@@ -1,2 +1,11 @@
 dict2=dict(((1,2),(2,4),(5,6)))
 print(dict2)
+dict1={101:'john',102:'smith',103:"ajay"}
+dict1[104]='david'
+print(dict1)
+dict1[101]='mark'
+print(dict1)
+del dict1[104]
+print(dict1)
+for i in dict1:
+    print(i,dict1[i])
