8e69a6e197a9f3eb59d308bcf7a7101216005cfd
Mahesh Yadav <mahesh767000@gmail.com>
2025-07-31 16:55:59 +0530

    Datetime

---
+++ Dateandtime/object.py
@@ -0,0 +1,10 @@
+from datetime import *
+dt1 =datetime.datetime(2010, 1, 1, 10, 10, 10)
+dt2 = datetime.datetime(day=1, month=1, year=2011, hour=10, minute=10, second=10)
+print(dt1)
+print(dt2)
+print(dt2-dt1)
+dat1=datetime.date(2010, 1, 1)
+tim1=datetime.time(10, 10, 10)
+dat3=datetime.datetime.combine(dat1, tim1)
+print(dat3)
